ee7dcc45dc404b532194a6048d9f17f7d02f85c2
Hui Li <lihui5126@gmail.com>
2019-07-04 21:57:17 -0700

    open and write files

---
+++ ex15.py
@@ -0,0 +1,16 @@
+#reading files
+#from sys import argv
+#script, filename = argv
+#txt = open(filename)
+
+#print(f"Here's your file {filename}: ")
+
+#print(txt.read())
+
+print("Type the filename again: ")
+file_again = input("> ")
+
+txt_again=open(file_again)
+#returns a file object
+print(txt_again.read())
+txt_again.close()
