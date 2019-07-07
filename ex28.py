2a3d536fb2a200d3dd55889b60d858522b37d3a9
Hui Li <lihui5126@gmail.com>
2019-07-07 13:45:53 -0700

    boolean practice

---
+++ ex28.py
@@ -0,0 +1,8 @@
+#Boolean practice
+"test" and "test"
+False and 1 # get False
+True and 1  #get 1
+
+#this is because Python and many languages like to return
+#one of the operands to their Boolean expressions rather than just
+#True or False.
