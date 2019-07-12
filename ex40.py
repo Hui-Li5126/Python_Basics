58c2ff9a445cdeab712173523a6657ee1aeecc80
Hui Li <lihui5126@gmail.com>
2019-07-11 21:35:39 -0700

    Classes introduction

---
+++ ex40.py
@@ -0,0 +1,11 @@
+#Modules, Classes, and Objects
+#Modules are a python file with some functions or variables in it....
+  #which you can import....
+  #And access the functions or varibles of with the . (dot) operator.
+  #think about a module as a specialized dictionary that can store Python code
+  #so you can access it with the . operator.
+
+#Class is a way to take a grouping of functions and data and
+  #place them inside a container so that you can access them
+  #with the . (dot) operator.
+  
