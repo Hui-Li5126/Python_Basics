4d15a3c217f0a00d05c39b5596ab9c870fac2125
Hui Li <lihui5126@gmail.com>
2019-08-01 20:29:12 -0700

    DateTime, TimeDelta, Strftime

---
+++ DateTime.py
@@ -0,0 +1,15 @@
+"""
+Python DateTime, TimeDelta, Strftime(Format) with examples
+In Python, date, time and datetime classes provides a number of function to deal
+with dates, times and time intervals. Date and datetime are an object in Python,
+so when you manipulate them, you are actually manipulating objects and not
+string or timestamps.
+
+The datetime classes in Python are categorized into main 5 classes.
+
+.date - Manipulate just date (Month, day, year)
+.time - Time independent of the day (Hour, minute, second, microsecond)
+.datetime - Combination of time and date
+.timedelta - A duration of time used for manipulating dates
+.tzinfo - An abstract class for dealing with time zones
+"""
